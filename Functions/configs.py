a71e9efa1dc358c4a3a0b5e3e0bd0436f4611dab
Luis Stella <luisstella.m@gmail.com>
2024-03-30 13:40:56 -0300

    Actualize functions

--- Functions/configs.py
+++ Functions/configs.py
@@ -1,10 +1,11 @@
 import sys
 import re
+from datetime import datetime
 
 DATABASE_PATH = "clients.db"
 
 if "pytest" in sys.argv[0]:
-    DATABASE_PATH = "tests/clientes_test.csv"
+    DATABASE_PATH = "Test/clientes_test.db"
 
 
 def valid_id(id, list):
@@ -14,4 +15,9 @@ def valid_id(id, list):
         if client.id == id:
             return False
     return True
- 
+
+def date():
+    date = datetime.now()
+    moment = datetime.strftime(date, "%d/%m/%Y")
+
+    return moment
--- Functions/database.py
+++ Functions/database.py
@@ -1,62 +1,69 @@
 import sqlite3 as sql
 import configs
+from datetime import datetime
 
 class Client:
-    def __init__(self, id, name, last_name, gender, age):
+    def __init__(self, id, name, surname, gender, age):
         self.id = id
         self.name = name
-        self.last_name = last_name
+        self.surname = surname
         self.gender = gender
         self.age = age
 
+    def __str__(self):
+        return '{} : {} {}'.format(self.id, self.name, self.surname)
+        
 class Clients:
-    #Generate/connect database
     connector = sql.connect(configs.DATABASE_PATH)
     cursor = connector.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS clients" \
-                   "(id VARCHAR PRIMARY KEY, name VARCHAR, last_name VARCHAR, gender VARCHAR, age INTEGER)")
+                   "(id VARCHAR PRIMARY KEY, name VARCHAR, surname VARCHAR, gender VARCHAR, age INTEGER)")
     
-    #List of clients/objects
     list_clients = []
 
-    #Add all clients in the list
     content = cursor.execute('SELECT * FROM clients').fetchall()
-    for id, name, last_name, gender, age in content:
-        client = Client(id, name, last_name, gender, age)
+    for id, name, surname, gender, age in content:
+        client = Client(id, name, surname, gender, age)
         list_clients.append(client) 
 
     connector.close()
 
     @staticmethod
-    def add(id, name, last_name, gender, age):
+    def search_client(id):
+        for client in Clients.list_clients:
+            if client.id == id:
+                return client
+
+    @staticmethod
+    def add_client(id, name, surname, gender, age):
         connector = sql.connect(configs.DATABASE_PATH)
         cursor = connector.cursor() 
 
-        client = Client(id, name, last_name, gender, age)
+        client = Client(id, name, surname, gender, age)
         Clients.list_clients.append(client) 
-        cursor.execute("INSERT INTO clients(id, name, last_name, gender, age) VALUES(?, ?, ?, ?, ?)", (client.id, client.name, client.last_name, client.gender, client.age))
+        cursor.execute("INSERT INTO clients(id, name, surname, gender, age) VALUES(?, ?, ?, ?, ?)", (client.id, client.name, client.surname, client.gender, client.age))
            
         connector.commit()
         connector.close()
     
     @staticmethod
-    def modificate(id, name, last_name, gender, age):
+    def modificate_client(id, name, surname, gender, age):
         connector = sql.connect(configs.DATABASE_PATH)
         cursor = connector.cursor() 
 
         for i, client in enumerate(Clients.list_clients):
             if client.id == id:
                 Clients.list_clients[i].name = name
-                Clients.list_clients[i].last_name = last_name
+                Clients.list_clients[i].surname = surname
                 Clients.list_clients[i].gender = gender
                 Clients.list_clients[i].age = age 
-                cursor.execute("UPDATE clients SET name = ?, last_name = ?, gender = ?, age = ? WHERE id = ?", (client.name, client.last_name, client.id, client.gender, client.age))
+                cursor.execute("UPDATE clients SET name = ?, surname = ?, gender = ?, age = ? WHERE id = ?", (client.name, client.surname, client.gender, client.age, client.id))
 
         connector.commit()
         connector.close()
             
     @staticmethod          
-    def remove(id):
+    def remove_client(id):
         connector = sql.connect(configs.DATABASE_PATH)
         cursor = connector.cursor()
 
@@ -69,24 +76,120 @@ class Clients:
         connector.close()
 
     @staticmethod
-    def add_many(clients):
+    def add_many_clients(list_new_clients):
         connector = sql.connect(configs.DATABASE_PATH)
         cursor = connector.cursor()
 
-        for any in clients:
+        for any in list_new_clients:
             client = Client(any[0], any[1], any[2], any[3], any[4])
             Clients.list_clients.append(client) 
-            cursor.execute("INSERT INTO clients(id, name, last_name, gender, age) VALUES(?, ?, ?, ?, ?)", (client.id, client.name, client.last_name, client.gender, client.age))
+            cursor.execute("INSERT INTO clients(id, name, surname, gender, age) VALUES(?, ?, ?, ?, ?)", (client.id, client.name, client.surname, client.gender, client.age))
 
         connector.commit()
         connector.close()
 
     @staticmethod
-    def remove_all(): 
+    def remove_all_clients(): 
         connector = sql.connect(configs.DATABASE_PATH)
         cursor = connector.cursor() 
 
         cursor.execute("DELETE FROM clients;")
+
+        connector.commit()
+        connector.close()
+
+class Sale:
+    def __init__(self, id, client_id, date, cash, transaction_state, service_state):
+        self.id = id
+        self.client_id = client_id
+        self.date = date
+        self.cash = cash
+        self.transaction_state = transaction_state
+        self.service_state = service_state
+    
+    def __str__(self):
+        return "N° {}, {} and {}".format(self.id, self.transaction_state, self.service_state)
+
+class Sales:
+    connector = sql.connect(configs.DATABASE_PATH)
+    cursor = connector.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS sales" \
+                   "(id INTEGER PRIMARY KEY AUTOINCREMENT, client_id VARCHAR, date VARCHAR, cash INTEGER, transaction_state VARCHAR, service_state VARCHAR, FOREIGN KEY (client_id) REFERENCES clients(id))")
+    
+    list_sales = []
+
+    content = cursor.execute("SELECT FROM * sales")
+
+    for id, client_id, date, cash, transaction_state, service_state in content:
+        sale = Sale(id, client_id, date, cash, transaction_state, service_state)
+        list_sales.append(sale)
+
+    connector.close()
+
+    @staticmethod
+    def search_sale(id):
+        for sale in Sales.list_sales:
+            if sale.id == id:
+                    return sale
+            
+    @staticmethod
+    def add_sale(id, client_id, date, cash, transaction_state, service_state):
+        connector = sql.connect(configs.DATABASE_PATH)
+        cursor = connector.cursor()
+        
+        sale = Sale(id, client_id, date, cash, transaction_state, service_state)
+        cursor.execute("INSERT INTO sales(id, client_id, date, cash, transaction_state, service_state) VALUES(?, ?, ?, ?, ?, ?)" (sale.id, sale.client_id, sale.date, sale.cash, sale.transaction_state, sale.service_state))
+
+        connector.commit()
+        connector.close()
+
+    @staticmethod
+    def modificate_sale(id, transaction_state, service_state):
+        connector = sql.connect(configs.DATABASE_PATH)
+        cursor = connector.cursor()
+
+        for i, sale in enumerate(Sales.list_sales):
+            if sale.id == id:
+                Clients.list_sales[i].transaction_state = transaction_state
+                Clients.list_sales[i].service_state = service_state
+                cursor.execute("UPDATE sales SET transaction_state = ?, service_state = ? WHERE id = ?", (sale.transaction_state, sale.service_state, sale.id))
+        
+        connector.commit()
+        connector.close()
+
+    @staticmethod
+    def remove_sale(id):
+        connector = sql.connect(configs.DATABASE_PATH)
+        cursor = connector.cursor()
+
+        for i, sale in enumerate(Sales.list_sales):
+            if sale.id == id:
+                Sales.list_sales.pop(i)
+                cursor.execute("DELETE FROM sales WHERE id = ?", (id,))
+
+        connector.commit()
+        connector.close()
+
+    @staticmethod
+    def add_many_sales(list_new_sales):
+        connector = sql.connect(configs.DATABASE_PATH)
+        cursor = connector.cursor()
+
+        for any in list_new_sales:
+            sale = Sale(any[0], any[1], any[2], any[3], any[4], any[5])
+            Sales.list_sales.append(sale)
+            cursor.execute("INSERT INTO sales(id, client_id, date, cash, transaction_state, service_state) VALUES(?, ?, ?, ?, ?, ?)", (sale.id, sale.client_id, sale.date, sale.cash, sale.transaction_state, sale.service_state))
+
+        connector.commit()
+        connector.close()
+
+    @staticmethod
+    def remove_all_sales():
+        connector = sql.connect(configs.DATABASE_PATH)
+        cursor = connector.cursor()
+
+        cursor.execute("DELETE FROM sales;")
 
         connector.commit()
         connector.close()
