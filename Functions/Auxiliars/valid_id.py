fcfec465ce7ddb861f3a1aa341e676f0bf580b9d
Luis Stella <luisstella.m@gmail.com>
2024-07-09 00:49:25 -0300

    Add regular expressions for all models

--- Functions/Auxiliars/valid_id.py
+++ Functions/Auxiliars/valid_id.py
@@ -1,7 +1,19 @@
 import re
 
-def valid_id(id, list):
-    if not re.match('[A-Z0-9]{3}-[A-Z0-9]$', id):
+person_id_pattern = '[A-Z0-9]{5}-[A-Z0-9]$'
+transfer_id_pattern = '[0-9]{8}[A-Z]$'
+
+id_patterns = {
+    'client_id_pattern': person_id_pattern,
+    'worker_id_pattern': person_id_pattern,
+    'sale_id_pattern': transfer_id_pattern,
+    'expense_id_pattern': transfer_id_pattern,
+    'task_id_pattern': '[A-Z]{8}[0-9]'
+}
+
+def valid_id(id, list, pattern_key):
+    pattern = id_patterns.get(pattern_key)
+    if not re.match(pattern, id):
         return False
     for any in list:
         if any.id == id:
--- Tests/test_sme.py
+++ Tests/test_sme.py
@@ -1,13 +1,12 @@
 from copy import copy
 from tempfile import NamedTemporaryFile
-from datetime import date
 import pandas as pd
 import os
 import unittest
 from openpyxl import Workbook
 from Functions.models import Clients, Client, Sales, Sale, Workers, Worker, Expenses, Expense
 from Functions.excel import SaveAllInExcel, LoadAllFromTable, SaveOneInExcel
-from Functions.Auxiliars import valid_id as vid
+from Functions.Auxiliars.valid_id import valid_id
 
 class TestModels:
     class TestModelClient(unittest.TestCase):
@@ -508,10 +507,18 @@ class TestAuxiliars(unittest.TestCase):
             self.test_db_file = NamedTemporaryFile(delete=False)
             self.db_file_name = self.test_db_file.name
             self.test_db_file.close()
+
             Clients.create_table_client(self.db_file_name)
-            Clients.add_client('ABC-1', 'John', 'Doe', 'Male', 30, 'tr@gmail.com', self.db_file_name)
-            Clients.add_client('XYZ-2', 'Alice', 'Smith', 'Female', 25, 'y@gmail.com', self.db_file_name)
-            Clients.add_client('123-3', 'Michael', 'Johnson', 'Male', 40, 'urs@gmail.com', self.db_file_name)
+            Clients.add_client('AB7DE-1', 'John', 'Doe', 'Male', 30, 'tr@gmail.com', self.db_file_name)
+
+            Sales.create_table_sale(self.db_file_name)
+            Sales.add_sale('00000876L', None, '2024-06-20 21:15:00', 80, 'completed', 'active', 'X5 Pasta Bolognese Combo', self.db_file_name)
+
+            Workers.create_table_worker(self.db_file_name)
+            Workers.add_worker('OPL67-5', 'Carlos', 'Quintero', 'Cashier', 550, 'plm@gmail.com', self.db_file_name)
+
+            Expenses.create_table_expense(self.db_file_name)
+            Expenses.add_expense('98052000B', None, '2008-02-03 20:21:00', 30, 'inactive', 'Recompose a client', self.db_file_name)
 
         def tearDown(self):
             try:
@@ -521,8 +528,26 @@ class TestAuxiliars(unittest.TestCase):
 
         def test_valid_id(self):
             list_clients = Clients.load_clients(self.db_file_name)
-            self.assertTrue(vid.valid_id('34W-W', list_clients))
-            self.assertFalse(vid.valid_id('75480', list_clients))
-            self.assertFalse(vid.valid_id('04G-a', list_clients))
-            self.assertFalse(vid.valid_id('5t6-y', list_clients))
-            self.assertFalse(vid.valid_id('ABC-1', list_clients))
+            list_sales = Sales.load_sales(self.db_file_name)
+            list_workers = Workers.load_workers(self.db_file_name)
+            list_expenses = Expenses.load_expenses(self.db_file_name)
+
+            self.assertTrue(valid_id('3456W-W', list_clients, 'client_id_pattern'))
+            self.assertFalse(valid_id('75480', list_clients, 'client_id_pattern'))
+            self.assertFalse(valid_id('04G5s-a', list_clients, 'client_id_pattern'))
+            self.assertFalse(valid_id('AB7DE-1', list_clients, 'client_id_pattern'))
+
+            self.assertTrue(valid_id('00000719J', list_sales, 'sale_id_pattern'))
+            self.assertFalse(valid_id('6676U650K', list_sales, 'sale_id_pattern'))
+            self.assertFalse(valid_id('3180M', list_sales, 'sale_id_pattern'))
+            self.assertFalse(valid_id('00000876L', list_sales, 'sale_id_pattern'))
+
+            self.assertTrue(valid_id('00004-Y', list_workers, 'worker_id_pattern'))
+            self.assertFalse(valid_id('870000', list_workers, 'worker_id_pattern'))
+            self.assertFalse(valid_id('a0Fj5-0', list_workers, 'worker_id_pattern'))
+            self.assertFalse(valid_id('OPL67-5', list_workers, 'worker_id_pattern'))
+
+            self.assertTrue(valid_id('88911000F', list_expenses, 'expense_id_pattern'))
+            self.assertFalse(valid_id('992YT890L', list_expenses, 'expense_id_pattern'))
+            self.assertFalse(valid_id('8600H', list_expenses, 'expense_id_pattern'))
+            self.assertFalse(valid_id('98052000B', list_expenses, 'expense_id_pattern'))
